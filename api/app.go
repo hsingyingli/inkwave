852938d674a4457b3b0b1ad4c2f5fe83c8793df3
hsingyingli <xingyingli855@gmail.com>
2025-02-16 14:48:21 +0800

    feat(App): register db to application

--- api/app.go
+++ api/app.go
@@ -1,24 +1,37 @@
 package api
 
 import (
+	"context"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/hsingyingli/inkwave-backend/api/routes"
+	db "github.com/hsingyingli/inkwave-backend/db/querier"
+	"github.com/jackc/pgx/v5"
 )
 
 type App struct {
 	app *fiber.App
+	db  *db.Queries
 }
 
 func NewApp() (*App, error) {
+	ctx := context.Background()
 	app := fiber.New()
+	conn, err := pgx.Connect(ctx, "user=pqgotest dbname=pqgotest sslmode=verify-full")
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close(ctx)
 
 	return &App{
 		app: app,
+		db:  db.New(conn),
 	}, nil
 }
 
 func (app *App) Initialize() error {
 	// create repository
+
 	// create service
 
 	// route
