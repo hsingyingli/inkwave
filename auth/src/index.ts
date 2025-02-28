5aa1b71a7957b71802fe0aef0e8e2148297c5e57
hsingyingli <xingyingli855@gmail.com>
2025-02-28 17:03:17 +0800

    build: init frontend, ai server and auth server

---
+++ auth/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(3000, () => {
+  console.log(`[server]: Server is running at http://localhost:3000`);
+});
